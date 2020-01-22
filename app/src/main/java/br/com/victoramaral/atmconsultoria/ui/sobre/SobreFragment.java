fa79d86149abb8d67da9c53643fd3777a246d241
Victor Amaral <vic_amaral_23@hotmail.com>
2020-01-22 01:03:37 -0300

    Adicionada página "Principal", "Serviços" e "Clientes".

---
+++ app/src/main/java/br/com/victoramaral/atmconsultoria/ui/sobre/SobreFragment.java
@@ -0,0 +1,32 @@
+package br.com.victoramaral.atmconsultoria.ui.sobre;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import br.com.victoramaral.atmconsultoria.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SobreFragment extends Fragment {
+
+
+    public SobreFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_sobre, container, false);
+    }
+
+}
